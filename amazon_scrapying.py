5aeb09e54b2867abdefb1334c40cf37eca57f413
hisao <hisao52321983@gmail.com>
2024-04-19 05:53:29 +0900

    複数ページ取得可能

--- amazon_scrapying.py
+++ amazon_scrapying.py
@@ -1,23 +1,19 @@
 from selenium import webdriver
-import time
-from time import sleep
 from selenium.webdriver.common.by import By
-import pandas as pd
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.chrome.options import Options
-
-# wait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+import os
+import urllib
+from time import sleep
+import time
+import chromedriver_binary
+from webdriver_manager.chrome import ChromeDriverManager
+driver=webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-# WebDriverのオプションを設定
 options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.add_argument("--no-sandbox")  # セキュリティ上の制限を回避
-options.add_argument("--lang=ja")     # 言語設定を日本語に
-
-# ChromeDriverを使用してWebDriverのインスタンスを作成
-driver = webdriver.Chrome(options=options)
+#options.add_argument("--headless")
+driver.get('https://www.google.com/imghp?hl=ja&tab=ri&ogbl')
 
 HREFS = []
 d_list = []
@@ -45,35 +41,32 @@ driver.find_element(By.ID,"nav-search-submit-button").click()
 wait.until(EC.presence_of_all_elements_located)
 
     #商品URLの取得
-#counter = 10
-while counter > 0:
+counter = 1
+while counter < 10:
     URLS = driver.find_elements(By.CSS_SELECTOR,"a.a-link-normal.s-no-outline")
+    print("エレメント取得")
     for URL in URLS:
         URL = URL.get_attribute("href")
-        print("[INFO] URL :", URL)
+        #print("[INFO] URL :", URL)
         HREFS.append(URL)
     wait.until(EC.presence_of_all_elements_located)
-    driver.find_element(By.CSS_SELECTOR,"#search > div.s-desktop-width-max.s-desktop-content.s-opposite-dir.s-wide-grid-style.sg-row > div.sg-col-20-of-24.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span.rush-component.s-latency-cf-section > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(78) > div > div > span > a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator").click()
+    print(f"URL取得{counter}")
+    next=driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/div[1]/div[1]/div/span[1]/div[1]/div[69]/div/div/span/a[3]")
+    print("next取得")
+    next_URL = next.get_attribute("href")
+    driver.get(next_URL)
     sleep(5)
-    counter = counter - 1  # counter -= 1 でも同等    
+    counter = counter + 1  
 print("Finish!!")
-
-"""URLS = driver.find_elements(By.CSS_SELECTOR,"a.a-link-normal.s-no-outline")
-
-for URL in URLS:
-    URL = URL.get_attribute("href")
-    print("[INFO] URL :", URL)
-    HREFS.append(URL)
-    #商品詳細の取得 
 
 for HREF in HREFS:
     driver.get(HREF)
     # title
     title = driver.find_element(By.ID, "productTitle").text
-    print("[INFO]  title :", title)
+    #print("[INFO]  title :", title)
     # price 
     price = driver.find_element(By.CSS_SELECTOR, 'div.aok-align-center > span > span > span.a-price-whole').text
-    print("[INFO]  price :", price)
+    #print("[INFO]  price :", price)
     # 複数画像取得
     images_btn = driver.find_elements(By.CSS_SELECTOR, "li.a-spacing-small.item.imageThumbnail.a-declarative > span > span > span > input")
     for index, image_btn in enumerate(images_btn, start=1):
@@ -104,5 +97,5 @@ df_URL = pd.DataFrame({'URL':HREFS})
 print(df_URL)
 df_concat= pd.concat([df, df_URL], axis=1)
 print(df_concat)
-df_concat.to_excel("amazon_keybord.xlsx")"""
+df_concat.to_excel("amazon_keybord.xlsx")
     
